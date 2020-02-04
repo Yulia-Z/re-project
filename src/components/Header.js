5a84c8b322ba9316805682d95fe57ba0670f20fe
Yulia-Z <yzavarik@gmail.com>
2020-02-03 21:51:44 -0800

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from "./Menu";
+import Logo from "./Logo";
+
+
+function Header() {
+  return (
+    <div>
+      <Logo/>
+      <Menu/>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MenuItem from "./MenuItem";
+
+function Menu() {
+  return (
+    <div>
+      <MenuItem/>
+      <MenuItem/>
+      <MenuItem/>
+      <MenuItem/>
+    </div>
+  );
+}
+
+export default Menu;
