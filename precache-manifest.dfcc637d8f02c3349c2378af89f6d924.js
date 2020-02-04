f62a872d57b4af26e5bc39d7bf70ac9de8878a93
Yulia-Z <yzavarik@gmail.com>
2020-02-03 21:45:26 -0800

    Updates

---
+++ precache-manifest.dfcc637d8f02c3349c2378af89f6d924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35dac16d22da3efa7ea2386659848c5e",
+    "url": "/re-project/index.html"
+  },
+  {
+    "revision": "23c91a8b654b4e80670b",
+    "url": "/re-project/static/css/main.61fcc705.chunk.css"
+  },
+  {
+    "revision": "14eb8d39164e72293efe",
+    "url": "/re-project/static/js/2.85205a00.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/re-project/static/js/2.85205a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23c91a8b654b4e80670b",
+    "url": "/re-project/static/js/main.d60db4f5.chunk.js"
+  },
+  {
+    "revision": "ff3155f398372a0ee153",
+    "url": "/re-project/static/js/runtime-main.66f7e858.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/re-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
